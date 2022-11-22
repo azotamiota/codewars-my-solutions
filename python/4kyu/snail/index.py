401c51c8a4078fa82a1b5f961141483e22247921
azotamiota <azotamiota@gmail.com>
2022-11-22 10:53:43 +0000

    add 4kyu solution

--- python/4kyu/snail/index.py
+++ python/4kyu/snail/index.py
@@ -1,38 +1,22 @@
 def snail(snail_map):
     steps = len([num for chunk in snail_map for num in chunk ])
-#     idx = end - 1
-    line_idx_right = 0
-    line_idx_left = len(snail_map) - 1
-    line_idx_down = len(snail_map) - 1
-    line_idx_up = 0
-    start = 0
-    end = len(snail_map)
-    vert_steps = len(snail_map) - 2
-#     numbers_to_add = len(snail_map)
+    top_and_left_idx = 0
+    bottom_and_right_idx = len(snail_map) - 1
+    vertical_steps = len(snail_map) - 2
     result = []
+    
     while len(result) < steps:
-        for i in range(start, end):
-            result.append(snail_map[line_idx_right][i])
-        print(result)
-        for i in range(vert_steps):
-            result.append(snail_map[start + i + 1][line_idx_down])
-        print(result)
-        for i in range(end):
-            result.append(snail_map[line_idx_left][::-1][i])
-        print(result)
+        for i in range(top_and_left_idx, bottom_and_right_idx + 1):
+            result.append(snail_map[top_and_left_idx][i])
+        for i in range(vertical_steps):
+            result.append(snail_map[top_and_left_idx + 1 + i][bottom_and_right_idx])
+        for i in range(top_and_left_idx, bottom_and_right_idx + 1):
+            result.append(snail_map[bottom_and_right_idx][::-1][i])
+        for i in range(vertical_steps):
+            result.append(snail_map[bottom_and_right_idx - 1 - i][top_and_left_idx])
+            
+        top_and_left_idx += 1
+        bottom_and_right_idx -= 1
+        vertical_steps -= 2
         
-        for i in range(vert_steps):
-            result.append(snail_map[end - 2 - i][line_idx_up])
-        print(result)
-        line_idx_right += 1
-        start += 1
-        end -= 1
-        line_idx_down -= 1
-        line_idx_up += 1
-        vert_steps -= 1
-    # print(result)
-
-snail([[1,2,3,4],
-       [5,6,7,8],
-       [9,10,11,12],
-       [13,14,15,16]])
+    return result if steps % 2 == 0 else result[:-1]
