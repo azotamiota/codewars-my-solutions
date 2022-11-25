84c4c76ab39b02c52d1a83642cdc998d4950162e
azotamiota <azotamiota@gmail.com>
2022-11-25 21:17:15 +0000

    add 4kyu kata

---
+++ python/4kyu/next_smaller_number/index.py
@@ -0,0 +1,12 @@
+# def next_smaller(n):
+#     print(n)
+#     smallest = ''.join(sorted([digit for digit in str(n)]))
+#     if smallest == str(n):
+#         return -1
+#     for i in range(n - 1, int(smallest) - 1, -1):
+#         if ''.join(sorted([digit for digit in str(i)])) == smallest:
+#             if len(str(i)) < len(str(n):
+#                 return -1
+#             return i
+
+print(set('stackkkklaaaaaaaa'))
