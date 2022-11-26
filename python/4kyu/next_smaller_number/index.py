c0c7bed46e0e4f28880aac7de1c89ddf9e2489c2
azotamiota <azotamiota@gmail.com>
2022-11-26 20:03:48 +0000

    another solution (still timed out)

--- python/4kyu/next_smaller_number/index.py
+++ python/4kyu/next_smaller_number/index.py
@@ -2,14 +2,18 @@ from itertools import permutations as ps
 
 def next_smaller(n):
 
-    perms = (''.join(p) for p in ps(str(n)) if int(''.join(p)) < n)
-    print(max(perms))
-    print(list(perms))
+    for i in range(2, len(str(n)) + 1):
+        tail = str(n)[-i:]
+        print('tail: ', tail)
+        perms = (''.join(p) for p in ps(str(tail))) #if int(''.join(p)) < n)
+        # print(sorted(perms, reverse=True))
+        for chunk in sorted(perms, reverse=True):
+            print('chunk: ', chunk)
+            curr_num = int(str(n)[:-i] + chunk)
+            print('curr_num: ', curr_num)
+            if curr_num < n and len(str(curr_num)) == len(str(n)):
+                return curr_num
+    return -1
 
-    # return set(perms(n))
+print(next_smaller(1234567908))
 
-
-next_smaller(4312)
-
-# for i in next_smaller(231):
-#     print(i)
