9f36bd2d6eb758b39b46443bad14b4368938c73d
azotamiota <azotamiota@gmail.com>
2022-11-26 18:52:05 +0000

    update idea

--- python/4kyu/next_smaller_number/index.py
+++ python/4kyu/next_smaller_number/index.py
@@ -1,12 +1,15 @@
-# def next_smaller(n):
-#     print(n)
-#     smallest = ''.join(sorted([digit for digit in str(n)]))
-#     if smallest == str(n):
-#         return -1
-#     for i in range(n - 1, int(smallest) - 1, -1):
-#         if ''.join(sorted([digit for digit in str(i)])) == smallest:
-#             if len(str(i)) < len(str(n):
-#                 return -1
-#             return i
+from itertools import permutations as ps
 
-print(set('stackkkklaaaaaaaa'))
+def next_smaller(n):
+
+    perms = (''.join(p) for p in ps(str(n)) if int(''.join(p)) < n)
+    print(max(perms))
+    print(list(perms))
+
+    # return set(perms(n))
+
+
+next_smaller(4312)
+
+# for i in next_smaller(231):
+#     print(i)
