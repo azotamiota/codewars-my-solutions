78e920f7f9bdf4a948094269b5842f6a79bfa289
azotamiota <azotamiota@gmail.com>
2022-11-27 19:59:50 +0000

    add 4kyu solution

--- python/4kyu/next_smaller_number/index.py
+++ python/4kyu/next_smaller_number/index.py
@@ -1,28 +1,18 @@
-from itertools import permutations as ps
-
 def next_smaller(n):
-    print(n)
     if sorted(list(str(n))) == list(str(n)):
         return -1
     lowest_digit = 9
-    for idx,digit in enumerate(str(n)[::-1]):
-        print('digit: ', digit)
+    breakpoint_num = 0
+    for idx,digit in enumerate(str(n)[::-1]): # looping through digits from behind
         if int(digit) <= lowest_digit:
             lowest_digit = int(digit)
-            print('lowest_digit: ', lowest_digit)
         else:
-            print('idx: ', idx)
-            tail = str(n)[-idx-1:]
-            print('tail: ', tail)
-            perms = (''.join(p) for p in ps(str(tail))) #if int(''.join(p)) < n)
-# print(sorted(perms, reverse=True))
-            for chunk in sorted(perms, reverse=True):
-                print('chunk: ', chunk)
-                curr_num = int(str(n)[:-idx-1] + chunk)
-                print('curr_num: ', curr_num)
-                if curr_num < n and len(str(curr_num)) == len(str(n)):
-                    return curr_num
-    return -1
-
-print(next_smaller(1234567908))
-
+            tail = str(n)[-idx-1:] # if there's a digit that is bigger than the previous one: There is a smaller number solution
+            breakpoint_num = tail[0]
+            sorted_tail_digits = ''.join(sorted(list(tail), reverse=True))
+            for i, digit in enumerate(sorted_tail_digits): # change tail digits to be the next smaller number with the same digits
+                if digit < breakpoint_num:
+                    desired_tail = digit + sorted_tail_digits[:i] + sorted_tail_digits[i + 1:] # concatenate the desired tail 
+                    if (str(n)[:-idx-1] + desired_tail)[0] != '0': # if result number don't start with '0'
+                        return int(str(n)[:-idx-1] + desired_tail)
+            return -1
