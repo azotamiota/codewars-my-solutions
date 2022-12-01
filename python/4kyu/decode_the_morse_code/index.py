51db20c28705b0c0cf7931c3e99905d1020ecff3
azotamiota <azotamiota@gmail.com>
2022-12-01 00:02:11 +0000

    add 4kyu solution (Morse decoder)

--- python/4kyu/decode_the_morse_code/index.py
+++ python/4kyu/decode_the_morse_code/index.py
@@ -1,24 +1,16 @@
 def decode_bits(bits):
-    print('bits: ', bits)
-    dot_length = min([len(elem) for elem in bits.split('0') if len(elem) > 0])
-#     print(dot_length * '111')
-    # ToDo: Accept 0's and 1's, return dots, dashes and spaces
-    morse = bits.replace(dot_length * '111', '-').replace(dot_length * '000', '   ').replace(dot_length * '1', '.').replace(dot_length * '0', ' ')
-    print('morse: ', morse.replace('0', ''))
-    return morse.replace('0', '')
+    bits = bits.strip('0') # delete head/tail zeroes
+    zeroes_length = min([len(elem) if len(elem) > 0 else float('inf') for elem in bits.split('0')])
+    ones_length = min([len(elem) if len(elem) > 0 else float('inf') for elem in bits.split('1')])
+    one_dot_length = zeroes_length if zeroes_length < ones_length else ones_length # This is the "time unit" length
+    morse = bits.replace(one_dot_length * '111', '-').replace(one_dot_length * '000', '   ').replace(one_dot_length * '1', '.').replace(one_dot_length * '0', ' ')
+    return morse
 
 def decode_morse(morseCode):
-    print('morseCode: ', morseCode)
-#     print(morseCode)
-    # ToDo: Accept dots, dashes and spaces, return human-readable message
     sentence = morseCode.split('       ')
-    print(sentence)
     result_arr = []
     for word in sentence:
         for letter in word.split('   '):
             result_arr.append(MORSE_CODE[letter.replace(' ','')])
         result_arr.append(' ')
-    print(''.join(result_arr))
-    return ''.join(result_arr).strip()
-#     return morseCode.replace('- . - -', MORSE_CODE['-.--'])
-#     return morseCode.replace('.', MORSE_CODE['.']).replace('-', MORSE_CODE['-']).replace(' ', '')
+    return ''.join(result_arr)[:-1] # delete last space
