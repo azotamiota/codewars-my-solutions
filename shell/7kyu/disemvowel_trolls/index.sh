276b946ac2a2c8cf266188e406d1294a012774d4
Norbert Majer <azotamiota@gmail.com>
2023-11-05 21:43:24 +0000

    clean code

--- shell/7kyu/disemvowel_trolls/index.sh
+++ shell/7kyu/disemvowel_trolls/index.sh
@@ -1,15 +1,3 @@
 #!/bin/bash -x
 
-# your code here #input text as $1,output as result
 echo $1 | sed 's/[aeiou]//ig'
-# tr -d 'AaEeIiOoUu' << "$1"
-# SOLUTION=""
-# for ((i=0; i<${#1}; i++)); do
-#     if [[ ${1:$i:1} != [aeiouAEOUI] ]]; then
-#       if [[ ${1:$i-1:2} != "  " ]]; then
-#         SOLUTION=$SOLUTION${1:$i:1}
-#       fi
-#     fi
-# done
-
-# echo $SOLUTION
