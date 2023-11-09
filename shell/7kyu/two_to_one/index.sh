55061c4014a245dd0a4de80d71d72ea6b072e56e
azotamiota <azotamiota@gmail.com>
2023-11-09 20:15:03 +0000

    change 7kyu solution

--- shell/7kyu/two_to_one/index.sh
+++ shell/7kyu/two_to_one/index.sh
@@ -1,15 +1,3 @@
 #!/bin/bash -x
 
-INPUT=$1$2
-OUTPUT=""
-SET=()
-for char in $(echo $INPUT | grep -o .); do
-    echo $char
-    if [[ "${SET[@]}" =~ $char ]]; then
-        :
-    else
-        SET+=$char
-        OUTPUT=$OUTPUT$char
-    fi
-done
-echo $OUTPUT | grep -o . | sort | tr -d '\n'
+echo $1$2 | grep -o . | sort -u | paste -sd ""
