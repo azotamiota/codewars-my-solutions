de5893df4d6cbc23fa397764299629a538f224f7
Norbert Majer <87644571+azotamiota@users.noreply.github.com>
2023-11-25 16:40:05 +0000

    add 7kyu solution

--- shell/7kyu/part_of_a_list/index.sh
+++ shell/7kyu/part_of_a_list/index.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+partlist() {
+spaces_count=$(wc -w <<< $1 | bc)
+solution=""
+for whitespace in $(seq 1 $(($spaces_count-1)) ); do
+    solution=${solution}\($(echo $1 | sed "s/ /,/$whitespace")\)
+done
+echo $solution
+}
+partlist "$1"
