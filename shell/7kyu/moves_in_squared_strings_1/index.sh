a56ab2de81c019f7bc4ac051a21f4c1c861e0dfa
Norbert Majer <87644571+azotamiota@users.noreply.github.com>
2023-11-26 17:29:15 +0000

    add 7kyu solution

--- shell/7kyu/moves_in_squared_strings_1/index.sh
+++ shell/7kyu/moves_in_squared_strings_1/index.sh
@@ -1,1 +1,24 @@
 #!/bin/bash
+IFS=","
+vertMirror() {
+    solution=""
+    for word in $1; do
+      solution=$solution"$(echo -n $word | rev)"$'\r' 
+    done
+    echo ${solution%?}
+}
+horMirror() {
+    solution=""
+    for word in $1; do
+      solution="$(echo -n $word)"'\r'"$solution"
+    done
+    echo ${solution%?}
+}
+oper() {
+    if [ "$1" == "vertMirror" ]; then
+        vertMirror "$2"
+    else
+        horMirror "$2"
+    fi
+}
+oper "$1" "$2"
