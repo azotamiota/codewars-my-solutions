4b1420285a7a292cae678d2c9131798f8d2d37a5
Norbert Majer <87644571+azotamiota@users.noreply.github.com>
2023-12-02 17:33:32 +0000

    add 7kyu solution

--- shell/7kyu/speed_control/index.sh
+++ shell/7kyu/speed_control/index.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+gps() {
+  max_average=0
+  averages=($(echo "$2" | sed "s/,//g"))
+  length=$(echo "${#averages[@]} - 1" | bc)
+  for d in $(seq 1 $length); do
+    distance=$(echo "${averages[$d]} - ${averages[$d - 1]}" | bc -l)
+    average=$(echo "3600 * $distance / $1" | bc)
+    if (( $average > $max_average )); then
+        max_average=$average
+    fi
+  done
+  echo $max_average
+}
+
+gps $1 "$2"
+
+####################### Test times out but logic works #########################
