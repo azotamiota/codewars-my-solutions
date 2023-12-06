253ffced8a614a577747a199ac8d6dbdb3a4cb8f
Norbert Majer <87644571+azotamiota@users.noreply.github.com>
2023-12-06 20:48:29 +0000

    add 7kyu solution

--- shell/7kyu/balanced_number/index.sh
+++ shell/7kyu/balanced_number/index.sh
@@ -2,9 +2,35 @@
 
 balancedNum ()
 {
-    local -- number=$1
-    
-    # Your code here
+ length=${#1}
+ if (( $length == 2 )); then
+    echo "Balanced"
+    exit
+ fi
+ half=$(( $length / 2 ))
+ first_sum=0
+ last_sum=0
+ if (( half % 2 == 1 )); then
+   for num in $(echo ${1:0:$half} | grep -o .); do
+        $(( first_sum+=$num ))
+   done
+   for num2 in $(echo ${1:$half+1} | grep -o .); do
+        $(( last_sum+=$num2 ))
+   done
+else
+   for num in $(echo ${1:0:$half-1} | grep -o .); do
+        $(( first_sum+=$num ))
+   done
+   for num2 in $(echo ${1:$half+1} | grep -o .); do
+        $(( last_sum+=$num2 ))
+   done
+fi
+(( $first_sum == $last_sum )) && echo "Balanced" || echo "Not Balanced"
+fi
 }
 
 balancedNum $1
+
+
+#   left_sum=$(echo "$left_sum" | sed 's/./&+/g' | sed 's/.$//')
+#   right_sum=$(echo "$right_sum" | sed 's/./&+/g' | sed 's/.$//')
