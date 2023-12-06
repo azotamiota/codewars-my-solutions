724b16203a1b999a87df3d4f26dec527175a2da7
azotamiota <azotamiota@gmail.com>
2023-12-06 23:10:57 +0000

    make solution loop free

--- shell/7kyu/balanced_number/index.sh
+++ shell/7kyu/balanced_number/index.sh
@@ -8,29 +8,16 @@ balancedNum ()
     exit
  fi
  half=$(( $length / 2 ))
- first_sum=0
- last_sum=0
+ first_half=0
+ last_half=0
  if (( half % 2 == 1 )); then
-   for num in $(echo ${1:0:$half} | grep -o .); do
-        $(( first_sum+=$num ))
-   done
-   for num2 in $(echo ${1:$half+1} | grep -o .); do
-        $(( last_sum+=$num2 ))
-   done
+   first_half=$(( $(echo ${1:0:$half} | sed 's/./&+/g' | sed 's/.$//') ))
+   last_half=$(( $(echo ${1:$half+1} | sed 's/./&+/g' | sed 's/.$//') ))
 else
-   for num in $(echo ${1:0:$half-1} | grep -o .); do
-        $(( first_sum+=$num ))
-   done
-   for num2 in $(echo ${1:$half+1} | grep -o .); do
-        $(( last_sum+=$num2 ))
-   done
+   first_half=$(( $(echo ${1:0:$half-1} | sed 's/./&+/g' | sed 's/.$//') ))
+   last_half=$(( $(echo ${1:$half+1} | sed 's/./&+/g' | sed 's/.$//') ))
 fi
-(( $first_sum == $last_sum )) && echo "Balanced" || echo "Not Balanced"
-fi
+(( $first_half == $last_half )) && echo "Balanced" || echo "Not Balanced"
 }
 
 balancedNum $1
-
-
-#   left_sum=$(echo "$left_sum" | sed 's/./&+/g' | sed 's/.$//')
-#   right_sum=$(echo "$right_sum" | sed 's/./&+/g' | sed 's/.$//')
