e04149fe8e28dc04c07caa29c99da8eb4070c53d
Norbert Majer <87644571+azotamiota@users.noreply.github.com>
2023-12-06 10:36:10 +0000

    Update solution

--- shell/7kyu/alphabetic_addition/index.sh
+++ shell/7kyu/alphabetic_addition/index.sh
@@ -3,5 +3,5 @@ for letter in $(echo $1 | grep -o .); do
   position=$(( $position + $(echo `expr index "abcdefghijklmnopqrstuvwxyz" $letter`) ))
 done
 position=$(( $position % 26 ))
-letters=('a' 'b' 'c' 'd' 'e' 'f' 'g' 'h' 'i' 'j' 'k' 'l' 'm' 'n' 'o' 'p' 'q' 'r' 's' 't' 'u' 'v' 'w' 'x' 'y' 'z')
+letters=({a..z})
 echo ${letters[$position - 1]}
