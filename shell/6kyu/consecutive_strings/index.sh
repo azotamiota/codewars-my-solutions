1436a56be80562531133db693dd69424f909c3f4
azotamiota <azotamiota@gmail.com>
2023-12-14 21:47:12 +0000

    add 6kyu solution

--- shell/6kyu/consecutive_strings/index.sh
+++ shell/6kyu/consecutive_strings/index.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 longestConsec() {
-    read -r -a strarr <<< "${1:1:-1}"
-for i in $(seq 0 ${#strarr[@]}); do
-    echo ${strarr[i]}${strarr[i+1]}
+read -r -a strarr <<< "$1"
+if (( ${#strarr[@]} == 0 )) || (( $2 > ${#strarr[@]} )) || (( $2 < 1 )); then
+    echo ""
+    exit
+fi
+loopcount=$(( ${#strarr[@]} - $2 ))
+final_concatenated=""
+for i in $(seq 0 $loopcount); do
+    current_concatenated=''
+    for j in $( seq 0 $(($2-1)) ); do
+        current_concatenated=$current_concatenated$(echo ${strarr[i+j]})
+    done    
+    if (( ${#current_concatenated} > ${#final_concatenated})); then
+        final_concatenated=$current_concatenated
+    fi
 done
-# echo $1
-# echo $2
+echo $final_concatenated
 }
 longestConsec "$1" $2
