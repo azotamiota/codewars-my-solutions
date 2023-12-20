36d93d6c5f72ce52efa52f8f8bd1d9ed21b72369
azotamiota <azotamiota@gmail.com>
2023-12-20 22:02:52 +0000

    add 6kyu challenge (unsolved)

---
+++ shell/6kyu/sums_of_parts/README.md
@@ -0,0 +1,3 @@
+# Sums of parts
+## Description
+[Codewars link](https://www.codewars.com/kata/5ce399e0047a45001c853c2b)
---
+++ shell/6kyu/sums_of_parts/index.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+sumsParts() {
+    result=""
+    addition=$(echo $1 | tr " " "+")
+    iterate=$(( ${#addition} + 1 ))
+    echo $addition
+    for i in $(seq 0 2 $iterate); do
+        number=$(echo "${addition:$i}" | bc)
+        result="$result$number "
+    done
+    echo "${result::-1}0"
+}
+sumsParts "$1"
