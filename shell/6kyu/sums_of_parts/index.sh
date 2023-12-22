9e4c628124c7aee1b25b9145cbe00aefaad82bfb
azotamiota <azotamiota@gmail.com>
2023-12-22 22:28:23 +0000

    add 6kyu solution (times out)

--- shell/6kyu/sums_of_parts/index.sh
+++ shell/6kyu/sums_of_parts/index.sh
@@ -1,14 +1,9 @@
-#!/bin/bash -x
 sumsParts() {
     result=""
-    addition=$(echo $1 | tr " " "+")
-    iterate=$(( ${#addition} + 1 ))
-    echo $addition
-    for i in $(seq 0 2 $iterate); do
-        number=$(echo "${addition:$i}" | bc)
-        result="$result$number "
-        echo $i
-    done
+    read -a array <<< "$1"
+    for i in $(seq 0 ${#array[@]}); do
+        result="$result$(echo ${array[@]:i} | tr " " "+" | bc) "
+    done 
     echo "${result::-1}0"
 }
 sumsParts "$1"
