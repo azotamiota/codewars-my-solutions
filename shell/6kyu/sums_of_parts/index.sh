961ef376e2d603fa5c01a18f24b41ddd0ff148f2
azotamiota <azotamiota@gmail.com>
2023-12-23 00:04:55 +0000

    update 6kyu solution

--- shell/6kyu/sums_of_parts/index.sh
+++ shell/6kyu/sums_of_parts/index.sh
@@ -1,9 +1,16 @@
 sumsParts() {
-    result=""
-    read -a array <<< "$1"
-    for i in $(seq 0 ${#array[@]}); do
-        result="$result$(echo ${array[@]:i} | tr " " "+" | bc) "
-    done 
-    echo "${result::-1}0"
+    [ "$1" == "" ] && echo "0" && exit
+    
+    read -a array <<< "$1" 
+    sums=()
+    total=$(echo ${array[@]} | tr " " "+" | bc)
+    sums[0]=$total          
+    
+    for (( i=1; i<=${#array[@]}; i++ )); do
+        (( sums[i] = sums[i-1] - array[i-1] ))
+    done
+
+    echo "${sums[@]}"
 }
+
 sumsParts "$1"
