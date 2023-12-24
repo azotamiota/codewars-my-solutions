15f612f08bca6dceb66cc7515bdc6ec80f7c9222
azotamiota <azotamiota@gmail.com>
2023-12-24 23:07:52 +0000

    add 6kyu solution

--- shell/6kyu/reverse_or_rotate/index.sh
+++ shell/6kyu/reverse_or_rotate/index.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 revrot () {
-    # your code
+    (( $2 <= 0 || $2 > ${#1} )) && echo "" && exit
+    chunks=$(( ${#1} / $2 ))
+    solution=""
+    for i in $(seq 0 $(( $chunks - 1 )) ); do
+        sum=0
+        start=$(( $i * $2 ))
+        chunk=${1:$start:$2}
+        for digit in $(echo $chunk | grep -o .); do
+            sum=$(( $sum + $digit ** 3 ))
+        done
+        if (( $sum % 2 == 0 )); then
+            solution=$solution$(echo $chunk | rev)
+        else
+            solution=$solution${chunk:1}${chunk::1}
+        fi
+    done 
+    echo $solution
 }
 revrot $1 $2
