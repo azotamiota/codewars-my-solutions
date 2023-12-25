b78f953695a1c4b12cd858f967c861fc83d62067
azotamiota <azotamiota@gmail.com>
2023-12-25 00:11:02 +0000

    add alternative solution to 6kyu kata

--- shell/6kyu/reverse_or_rotate/index.sh
+++ shell/6kyu/reverse_or_rotate/index.sh
@@ -19,3 +19,30 @@ revrot () {
     echo $solution
 }
 revrot $1 $2
+
+############################# Another solution ###############################
+
+#!/bin/bash
+revrot () {
+    (( $2 <= 0 || $2 > ${#1} )) && echo "" && exit
+    solution=""
+    solution=$(echo $1 | fold -w$2 | {
+        while read -r line; do
+        if (( ${#line} == $2 )); then
+            sum=0
+            for digit in $(echo $line | grep -o .); do
+                sum=$(( $sum + $digit ** 3 ))
+            done
+            if (( $sum % 2 == 0 )); then
+                # solution=$solution$(echo $line | rev)
+                echo -n $(echo $line | rev)
+            else
+                # solution=$solution${line:1}${line::1}
+                echo -n ${line:1}${line::1}
+            fi
+        fi
+    done
+    })
+    echo "$solution"
+}
+revrot $1 $2
