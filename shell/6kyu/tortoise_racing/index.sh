37bc79243197b5d2b91d1c8a040e9965b821a691
azotamiota <azotamiota@gmail.com>
2023-12-27 11:00:19 +0000

    update 6kyu challenge

--- shell/6kyu/tortoise_racing/index.sh
+++ shell/6kyu/tortoise_racing/index.sh
@@ -3,15 +3,16 @@ race() {
     (( $1 >= $2 )) && echo "None"
     first_position=$(( $3 * 1000 ))
     second_position=0
-    milliseconds_to_catch=0
-    while [[ echo "$second_position < $first_position" | bc -l ]]; do
-        (( milliseconds_to_catch++ ))
-        first_position=$(echo "$first_position + $1 * 1000 / 3600000" | bc -l )
-        second_position=$(echo "$second_position  + $2 * 1000 / 3600000" | bc -l)
+    seconds_to_catch=0
+    while (( $second_position < $first_position )); do
+        (( seconds_to_catch++ ))
+        first_position=$(( $first_position + $1 * 1000 / 3600 ))
+        second_position=$(( $second_position  + $2 * 1000 / 3600 ))
     done
-    hours=$(( $milliseconds_to_catch / 3600000 ))
-    minutes=$(( $milliseconds_to_catch % 3600000 / 60000))
-    seconds=$(( $milliseconds_to_catch % 60000 / 1000 ))
+    hours=$(( $seconds_to_catch / 3600 ))
+    minutes=$(( $seconds_to_catch % 3600 / 60))
+    seconds=$(( $seconds_to_catch % 60 ))
     echo $hours $minutes $seconds
 }
-race $1 $2 $3
+race $1 $2 $3 
+# 720 850 70
