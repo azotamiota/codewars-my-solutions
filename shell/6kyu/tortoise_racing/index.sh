2f0900463d2df123a4ff289377c5108b613f66fd
azotamiota <azotamiota@gmail.com>
2023-12-27 11:42:39 +0000

    add 6 kyu solution (times out)

--- shell/6kyu/tortoise_racing/index.sh
+++ shell/6kyu/tortoise_racing/index.sh
@@ -1,18 +1,20 @@
 #!/bin/bash -x
 race() {
     (( $1 >= $2 )) && echo "None"
-    first_position=$(( $3 * 1000 ))
+    first_position=$3
     second_position=0
+    first_distance_per_second=$(echo "$1 / 3600" | bc -l)
+    second_distance_per_second=$(echo "$2 / 3600" | bc -l)
     seconds_to_catch=0
-    while (( $second_position < $first_position )); do
+    while (( $(echo "$second_position < $first_position" | bc -l) )); do
         (( seconds_to_catch++ ))
-        first_position=$(( $first_position + $1 * 1000 / 3600 ))
-        second_position=$(( $second_position  + $2 * 1000 / 3600 ))
+        first_position=$( echo "$first_position + $first_distance_per_second" | bc -l )
+        second_position=$( echo "$second_position  + $second_distance_per_second" | bc -l )
     done
     hours=$(( $seconds_to_catch / 3600 ))
-    minutes=$(( $seconds_to_catch % 3600 / 60))
+    minutes=$(( ($seconds_to_catch % 3600) / 60))
     seconds=$(( $seconds_to_catch % 60 ))
-    echo $hours $minutes $seconds
+    echo $hours $minutes $(( seconds - 1 ))
 }
 race $1 $2 $3 
-# 720 850 70
+# input: 720 850 70
