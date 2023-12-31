8b3c28d35e6c8fd5a4c25dbea6b2c28435a630aa
azotamiota <azotamiota@gmail.com>
2023-12-31 21:24:06 +0000

    update 6kyu solution

--- shell/6kyu/playing_with_passphrases/index.sh
+++ shell/6kyu/playing_with_passphrases/index.sh
@@ -1,1 +1,29 @@
-echo $1
+#!/bin/bash
+string=$1
+shift_amount=$2
+
+# Initialize an empty result string
+result=""
+ascii_A=$(printf "%d" "'A") # ASCII value of 'A'
+
+
+for (( i=0; i<${#string}; i++ )); do
+    char="${string:$i:1}"
+    ascii_val=$(printf "%d" "'$char")
+    shifted_ascii_val=$(( (ascii_val - ascii_A + shift) % 26 + ascii_A ))
+
+    if [[ $char =~ [A-Z] ]]; then
+        if (( $i & 1 )); then
+            result+="\\$(printf "%o" "${shifted_ascii_val,,}")"
+        else
+            result+="\\$(printf "%o" "$shifted_ascii_val")"
+        fi
+    elif [[ $char =~ [0-9] ]]; then
+        complement=$(( 9 - $char ))
+        result+=$complement
+    else
+        result+=$char
+    fi
+done
+
+echo "$result"
