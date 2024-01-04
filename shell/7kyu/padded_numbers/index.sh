93b844f7486cfa456e180f58a9e16e548b451922
azotamiota <azotamiota@gmail.com>
2024-01-04 22:44:24 +0000

    update for passing tests

--- shell/7kyu/padded_numbers/index.sh
+++ shell/7kyu/padded_numbers/index.sh
@@ -1,6 +1,7 @@
 digit_count=${#1}
 zeroes_needed=$(( 5 - digit_count ))
+(( $zeroes_needed < 1 )) && echo "Value is $1" && exit
 zeroes=$(printf '0%.0s' $(seq 1 $zeroes_needed))
-echo $zeroes$1
+echo "Value is $zeroes$1"
 
 # printf "Value is %05d" $1
