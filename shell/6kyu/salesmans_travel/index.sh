8321a7b99caf985f8e8a45e157ae476614efd62d
azotamiota <azotamiota@gmail.com>
2024-01-09 22:45:34 +0000

    extract house number from input

--- shell/6kyu/salesmans_travel/index.sh
+++ shell/6kyu/salesmans_travel/index.sh
@@ -1,11 +1,13 @@
-#!/bin/bash -x
+#!/bin/bash
 travel() {
 local IFS=','
 local solution="$2:"
+house_numbers=""
 for address in $1; do
     if [[ "${address: -8}" == "$2" ]]; then
-        echo $address
-        solution+="${address::-9},"
+        echo $address | cut -d " " -f 2
+        house_numbers+="$(echo $address | cut -d " " -f 1)@"
+        solution+="${address::-9}@"
     fi
 done
 echo $solution
