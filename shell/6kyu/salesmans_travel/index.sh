5e89d4972740392cf2edfcdaee5c1c2530fc5fbf
azotamiota <azotamiota@gmail.com>
2024-01-09 23:35:15 +0000

    input formatting

--- shell/6kyu/salesmans_travel/index.sh
+++ shell/6kyu/salesmans_travel/index.sh
@@ -1,15 +1,21 @@
-#!/bin/bash
+#!/bin/bash -x
 travel() {
+full_addresses=$(echo $1 | sed "s/\n//g")
 local IFS=','
 local solution="$2:"
 house_numbers=""
-for address in $1; do
-    if [[ "${address: -8}" == "$2" ]]; then
-        echo $address | cut -d " " -f 2
-        house_numbers+="$(echo $address | cut -d " " -f 1)@"
-        solution+="${address::-9}@"
+addresses=""
+for address in $full_addresses; do
+    house_number=$(echo -n $address | cut -d " " -f 1)
+    zip_code="${address: -8}"
+    address_without_hn="${address#$house_number }"
+    address_without_hn_and_zipcode="${address_without_hn::-9}"
+    if [[ $zip_code == "$2" ]]; then
+        house_numbers+="$house_number@"
+        addresses+=$address_without_hn_and_zipcode@
     fi
 done
-echo $solution
+solution+="${addresses%?}/${house_numbers%?}"
+echo $solution | tr "@" ","
 }
 travel "$1" "$2"
