4f258ee9735d4687d5bf03eff47f182514dd5633
azotamiota <azotamiota@gmail.com>
2024-01-13 00:23:29 +0000

    update 6kyu kata

--- shell/6kyu/price_draw/index.sh
+++ shell/6kyu/price_draw/index.sh
@@ -1,5 +1,26 @@
-#!/bin/bash
+#!/bin/bash -x
 rank () {
-    # your code
+declare -A letter_ranks=(
+    [A]=1 [B]=2 [C]=3 [D]=4 [E]=5 [F]=6 [G]=7 [H]=8 [I]=9 [J]=10
+    [K]=11 [L]=12 [M]=13 [N]=14 [O]=15 [P]=16 [Q]=17 [R]=18 [S]=19
+    [T]=20 [U]=21 [V]=22 [W]=23 [X]=24 [Y]=25 [Z]=26
+)
+IFS=","
+winning_numbers=""
+name_position=0
+formatted_weight=$(echo "$2" | sed "s/,//g")
+for name in $1;do
+    name_uppercase=${name^^}
+    current_number=$(( ${#name} ))
+    for ((i=0; i<${#name_uppercase}; i++)); do
+        letter=${name_uppercase:$i:1}
+        (( current_number += ${letter_ranks[$letter]} ))
+    done
+    current_weight=${formatted_weight:name_position:1}
+    current_number=$(( current_number * current_weight))
+    winning_numbers+="$current_number,"
+    (( name_position++))
+done
+echo "$winning_numbers" | grep -oE "[^,]+" | sort #####  How to sort the final numbers and find the associated name?
 }
 rank "$1" "$2" "$3"
