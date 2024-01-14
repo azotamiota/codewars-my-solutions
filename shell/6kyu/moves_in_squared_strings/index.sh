a06e082375c3b4aaa5bcfc761b009beea9a8a0c5
azotamiota <azotamiota@gmail.com>
2024-01-14 23:11:13 +0000

    update 6kyu kata

--- shell/6kyu/moves_in_squared_strings/index.sh
+++ shell/6kyu/moves_in_squared_strings/index.sh
@@ -3,8 +3,8 @@ rot() {
     echo $1 | rev | sed "s/,/\r/g" # sed "s/,/@/g"
 }
 selfieAndRot() {
-    echo -n "${1}....\r...." | sed "s/,/..../g"
-    echo $1 | rev | sed "s/,/..../g"
+    echo -n "${1}....\r...." | sed "s/,/....\r/g"
+    echo $1 | rev | sed "s/,/\r..../g"
 }
 oper() {
     [[ $1 == "rot" ]] && rot $2 || selfieAndRot $2 # your code
