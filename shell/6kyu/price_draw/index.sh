2d527774691d64f3f16bb669852d542113a4173e
azotamiota <azotamiota@gmail.com>
2024-01-14 00:06:10 +0000

    add 6kyu solution

--- shell/6kyu/price_draw/index.sh
+++ shell/6kyu/price_draw/index.sh
@@ -1,12 +1,7 @@
-#!/bin/bash -x
+#!/bin/bash
 rank () {
-declare -A letter_ranks=(
-    [A]=1 [B]=2 [C]=3 [D]=4 [E]=5 [F]=6 [G]=7 [H]=8 [I]=9 [J]=10
-    [K]=11 [L]=12 [M]=13 [N]=14 [O]=15 [P]=16 [Q]=17 [R]=18 [S]=19
-    [T]=20 [U]=21 [V]=22 [W]=23 [X]=24 [Y]=25 [Z]=26
-)
+    [[ $1 == "" ]] && echo "No participants" && exit
 IFS=","
-winning_numbers=""
 name_position=0
 formatted_weight=$(echo "$2" | sed "s/,//g")
 for name in $1;do
@@ -14,13 +9,15 @@ for name in $1;do
     current_number=$(( ${#name} ))
     for ((i=0; i<${#name_uppercase}; i++)); do
         letter=${name_uppercase:$i:1}
-        (( current_number += ${letter_ranks[$letter]} ))
+        letter_rank=$(eval echo {A..${letter}} | tr " " "\n" | wc -l)
+        (( current_number += ${letter_rank} ))
     done
     current_weight=${formatted_weight:name_position:1}
     current_number=$(( current_number * current_weight))
-    winning_numbers+="$current_number,"
+    winning_number_and_name+="$current_number@$name,"
     (( name_position++))
 done
-echo "$winning_numbers" | grep -oE "[^,]+" | sort #####  How to sort the final numbers and find the associated name?
+winner=$(echo "$winning_number_and_name" | grep -oE "[^,]+" | sort -t'@' -k1,1nr -k2,2 | sed -n "${3}p" | cut -d@ -f 2)
+[[ $winner == "" ]] && echo "Not enough participants" || echo $winner 
 }
 rank "$1" "$2" "$3"
