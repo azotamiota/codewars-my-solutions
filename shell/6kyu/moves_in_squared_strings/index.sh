51cd974b35f29e8ac138a05948d9ec857af9b8c5
azotamiota <azotamiota@gmail.com>
2024-01-16 20:59:14 +0000

    add 6kyu solution

--- shell/6kyu/moves_in_squared_strings/index.sh
+++ shell/6kyu/moves_in_squared_strings/index.sh
@@ -3,10 +3,15 @@ rot() {
     echo $1 | rev | sed "s/,/\r/g" # sed "s/,/@/g"
 }
 selfieAndRot() {
-    echo -n "${1}....\r...." | sed "s/,/....\r/g"
-    echo $1 | rev | sed "s/,/\r..../g"
+    selfie=$(echo -n "${1}" | sed "s/,/${dots_needed}\r/g")
+    rot=$(echo $1 | rev | sed "s/,/\r${dots_needed}/g")
+    echo "$selfie@$rot" | sed "s/@/${dots_needed}\r${dots_needed}/g"
 }
 oper() {
     [[ $1 == "rot" ]] && rot $2 || selfieAndRot $2 # your code
 }
+length=$(echo "$2" | sed "s/[^,]//g") 
+length=$(( ${#length} + 1 ))
+dots_needed=$(printf '.%.0s' $(seq 1 $length))
+
 oper $1 $2
