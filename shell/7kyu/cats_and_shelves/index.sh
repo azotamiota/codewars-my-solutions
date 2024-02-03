69767b2bd2fdb3265fc1b94c5c98cf197d31c716
Norbert Majer <87644571+azotamiota@users.noreply.github.com>
2024-02-03 15:45:06 +0000

    add 7kyu solution

--- shell/7kyu/cats_and_shelves/index.sh
+++ shell/7kyu/cats_and_shelves/index.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-
 cats_shelves() {
-
+current_shelf=$1
+target_shelf=$2
+jumps=0
+while (( $current_shelf < $target_shelf - 1 )); do
+  (( current_shelf+=3 ))
+  (( jumps++ ))
+done
+(( $current_shelf == $target_shelf )) && echo $jumps || echo $(( $jumps + 1 ))
 }
 
-cats_shelves "$@"
+cats_shelves $1 $2
